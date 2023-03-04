2b9a7e2422b1f150707b7cec8b1f7c38ec94dada
buzum <buzum@mail.ru>
2023-03-04 13:11:12 +0400

    task 23 for HW 3

---
+++ task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+static void InitNumberByUser(ref int userNumber)
+{
+    try
+    {
+        Console.Write("Введите целое число: ");
+        userNumber = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных {exc.Message}");
+        return;
+    }
+}
+
+////// сама программа
+Console.Clear();
+
+int userNumber = 0;
+InitNumberByUser(ref userNumber);
+
+///////
+
+for (int index = 1; index <= userNumber; index++)
+{
+    Console.Write($"{Math.Pow(index, 3)} ");
+}
